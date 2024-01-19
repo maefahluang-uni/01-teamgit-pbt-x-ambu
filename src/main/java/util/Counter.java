84b84854589d068aa6eb9e6345f2981cb3368f6e
Weerapat6431503056 <97898698+Weerapat6431503056@users.noreply.github.com>
2024-01-19 09:02:53 +0000

    feature3

--- src/main/java/util/Counter.java
+++ src/main/java/util/Counter.java
@@ -31,28 +31,82 @@ public class Counter {
 
 	// TODO: dev1- method for increment to closest even number
 	public void incrementToEven() {
-		_ctr = -99;
+		increment();
+		// Check if the current value of _ctr is even
+		if (_ctr % 2 == 0) {
+			// Already even, no need to increment
+			System.out.println("Counter is already even.");
+		} else {
+			// Increment to the closest even number
+			_ctr = (_ctr % 2 == 0) ? _ctr : _ctr + 1;
+			System.out.println("Incremented to the closest even number.");
+		}
 	}
 
 	// TODO: dev1- method for decrement to closest even number
 	public void decrementToEven() {
-		_ctr = -99;
+		decrement();
+		// Check if the current value of _ctr is even
+		if (_ctr % 2 == 0) {
+			// Already even, no need to decrement
+			System.out.println("Counter is already even.");
+		} else {
+			// Decrement to the closest even number
+			_ctr = (_ctr % 2 == 0) ? _ctr : _ctr - 1;
+			System.out.println("Decremented to the closest even number.");
+		}
 	}
+
 
 	// TODO: dev2- method for increment to closest prime number
 	public void incrementToPrime() {
-		_ctr = -99;
+		increment();
+		while (!isPrime(_ctr)) {
+            _ctr++;
+        }
 	}
 
 	// TODO: dev2- method for decrement to closest prime number
 	public void decrementToPrime() {
-		_ctr = -99;
+		decrement();
+		while (!isPrime(_ctr)) {
+            _ctr--;
+        }
 	}
+
+
+	 // Check if a number is prime
+	 private boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 	// TODO: dev3- count the frequency of word in sentence,
 	// refactor source code from dev1 and dev2
 	public void countFrequency(String word, String sentence) {
-		_ctr = -99;
+        // Check for null or empty inputs
+        if (word == null || sentence == null || sentence.isEmpty()) {
+            System.out.println("Invalid input");
+            _ctr = -1;  // Or set it to another meaningful value for invalid input
+            return;
+        }
+		_ctr = 0;
+		// Split the sentence into words
+        String[] words = sentence.split("\\s+");
+
+        // Count the frequency of the given word
+        for (String w : words) {
+            if (word.equalsIgnoreCase(w)) {
+                _ctr++;
+            }
+        }
 	}
 
 }
